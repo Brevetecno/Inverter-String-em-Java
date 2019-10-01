d675a35b2410f0607f841de65fca8c91a167f178
Herberth Souza <51496746+Brevetecno@users.noreply.github.com>
2019-10-01 09:08:38 -0300

    Classe Java para inverter String
    Com essa classe pode se inverter qualquer texto da direita para esquerda

--- InverterString.java
+++ InverterString.java
@@ -13,17 +13,19 @@ public class InverterString{
 		String inverted_name = "";
 		
 		// Inverte uma String (No caso é a String text)
-        for(int i = normal_name.length()-1; i >= 0; i--){
+		for(int i = normal_name.length()-1; i >= 0; i--){
+
+			// Pega apenas uma letra da direita para esquerda da variável normal_name
+			char c = normal_name.charAt(i);
+
+			// Irá adicionando letra por letra da direita para esquerda dentro da inverted_name 
+			inverted_name += Character.toString(c);
+		}
 			
-                // Pega apenas uma letra da direita para esquerda da variável normal_name
-				char c = normal_name.charAt(i);
-                
-				// Irá adicionando letra por letra da direita para esquerda dentro da inverted_name 
-                inverted_name += Character.toString(c);
-				
-            }
+		// Mostra o texto normal
+		System.out.println("Texto normal: " + normal_name);
             
-			// Mostra o resultado final
-            System.out.println(inverted_name);
+		// Mostra o texto invertido
+        	System.out.println("Texto invertido: " + inverted_name);
 	}
 }
